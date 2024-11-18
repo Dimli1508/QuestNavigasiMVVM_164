259f7e2fcb7e4a49b37d8a307f81c87240710c8d
Mashell0707 <merlsutama@gmail.com>
2024-11-18 22:33:54 +0700

    memasukkan variabel email

--- app/src/main/java/com/example/m7/model/Mahasiswa.kt
+++ app/src/main/java/com/example/m7/model/Mahasiswa.kt
@@ -3,5 +3,7 @@ package com.example.m7.model
 data class Mahasiswa(
     val nama : String ="",
     val gender : String ="",
-    val alamat : String =""
+    val alamat : String ="",
+    val email : String ="",
+
 )
